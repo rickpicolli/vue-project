fabbb6f7b36840788c5d1021a456d3429de8e753
Ricardo Alexandre Pinheiro Picolli <rickpicolli@Mac.local>
2020-12-07 16:22:01 -0800

    Vue project

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,37 @@
+<template>
+<form @submit.prevent="addTodo">
+    <h2 class="label-wrapper">
+        <label for="new-todo-input" class="label__lg">
+        What needs to be done?
+    </label>
+    </h2>
+    <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model.lazy.trim="label" class="input__lg" />
+    <button type="submit" class="btn btn__primary btn__lg"> Add </button> 
+</form>
+  
+</template>
+
+<script>
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            label: ""
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+
+            if(this.label === "") {
+                return;
+            }
+            this.$emit('todo-added', this.label);
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,35 @@
+
+<template>
+<div>
+    <input type="checkbox" class="checkbox" id="id" :checked="isDone" @change="$emit('checkbox-changed')" />
+    <label :for="id">{{label}}</label>
+</div>
+  
+</template>
+
+<script>
+// import uniqueId from 'lodash.uniqueid';
+
+export default {
+    props: {
+        label: { required: true, type: String },
+        done: { default: false, type: Boolean },
+        id: { required: true, type: String }
+    },
+    data() {
+        return {
+            isDone: this.done
+            // id: uniqueId('todo-')
+        };
+    },
+};
+</script>
+
+<style scoped>
+
+    .checkbox {
+        margin-right: 20px;
+        width: 22px;
+        height: 22px;
+    }
+</style>
